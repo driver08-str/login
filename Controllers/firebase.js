88404e96ff8785e654eb548a6ad58a0d646aea38
driver08-str <159456755+driver08-str@users.noreply.github.com>
2024-03-20 23:40:01 +0000

    codigo de la rama main

---
+++ Controllers/firebase.js
@@ -0,0 +1,45 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js'
+
+import { 
+    getAuth, 
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged  
+} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpLI0TuSuToaYqpMr5KLoaf0GWkPAPEYM",
+  authDomain: "apiweb2024a.firebaseapp.com",
+  projectId: "apiweb2024a",
+  storageBucket: "apiweb2024a.appspot.com",
+  messagingSenderId: "576157774878",
+  appId: "1:576157774878:web:e73eaf877836f18083aa0f",
+  measurementId: "G-QXGEZF7Q2W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+//metodo de autenticacion de usuario
+export const loginauth=(email,password)=>
+    signInWithEmailAndPassword(auth, email, password)
+
+//cerrar sesion del usuario
+export const loginout=()=>
+  signOut(auth)
+
+//estado del usuario logeado
+export function userstate(){
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(uid)
+    } else {
+      window.location.href='../index.html'
+    }
+  });
+}
